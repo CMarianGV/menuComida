8aad96a7fc0801da955a23d56551d29526bdb431
CMarianGV <cruzmariangv@gmail.com>
2017-05-11 19:34:20 -0500

    quiero tener el correcto

--- js/app.js
+++ js/app.js
@@ -45,12 +45,4 @@ function checarElementos(){
 
 //-------------------------------------------FIREBASE-----------------------------------------------------
 // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyCoP1JuA4KkvgtJPx5II1j9C2vV9sbak8o",
-    authDomain: "menu-restaurante.firebaseapp.com",
-    databaseURL: "https://menu-restaurante.firebaseio.com",
-    projectId: "menu-restaurante",
-    storageBucket: "menu-restaurante.appspot.com",
-    messagingSenderId: "327373634763"
-  };
-  firebase.initializeApp(config);
+  
